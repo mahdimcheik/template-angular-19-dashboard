c4ece4ca785897b5a88115c65fa0b168b879e5fb
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-02-22 22:46:20 +0100

    first commit

---
+++ src/app/layout/component/menu/app.menu.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+    selector: 'app-menu',
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    templateUrl: './app.menu.html'
+})
+export class AppMenu {
+    model: MenuItem[] = [];
+
+    ngOnInit() {
+        this.model = [
+            { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] },
+
+            { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout'] },
+            { label: 'Input', icon: 'pi pi-fw pi-check-square', routerLink: ['/uikit/input'] },
+            { label: 'Button', icon: 'pi pi-fw pi-mobile', class: 'rotated-icon', routerLink: ['/uikit/button'] },
+            { label: 'Table', icon: 'pi pi-fw pi-table', routerLink: ['/uikit/table'] },
+            { label: 'List', icon: 'pi pi-fw pi-list', routerLink: ['/uikit/list'] },
+            { label: 'Tree', icon: 'pi pi-fw pi-share-alt', routerLink: ['/uikit/tree'] },
+            { label: 'Panel', icon: 'pi pi-fw pi-tablet', routerLink: ['/uikit/panel'] },
+            { label: 'Overlay', icon: 'pi pi-fw pi-clone', routerLink: ['/uikit/overlay'] },
+            { label: 'Media', icon: 'pi pi-fw pi-image', routerLink: ['/uikit/media'] },
+            { label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'] },
+            { label: 'Message', icon: 'pi pi-fw pi-comment', routerLink: ['/uikit/message'] },
+            { label: 'File', icon: 'pi pi-fw pi-file', routerLink: ['/uikit/file'] },
+            { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/uikit/charts'] },
+            { label: 'Timeline', icon: 'pi pi-fw pi-calendar', routerLink: ['/uikit/timeline'] },
+            { label: 'Misc', icon: 'pi pi-fw pi-circle', routerLink: ['/uikit/misc'] },
+
+            {
+                label: 'Login',
+                icon: 'pi pi-fw pi-sign-in',
+                routerLink: ['/auth/login']
+            },
+            {
+                label: 'Error',
+                icon: 'pi pi-fw pi-times-circle',
+                routerLink: ['/auth/error']
+            },
+            {
+                label: 'Access Denied',
+                icon: 'pi pi-fw pi-lock',
+                routerLink: ['/auth/access']
+            }
+        ];
+    }
+
+    itemClick(item: MenuItem) {
+        console.log(item);
+
+        // avoid processing disabled items
+        // if (this.item.disabled) {
+        //     event.preventDefault();
+        //     return;
+        // }
+
+        // // execute command
+        // if (this.item.command) {
+        //     this.item.command({ originalEvent: event, item: this.item });
+        // }
+
+        // // toggle active state
+        // if (this.item.items) {
+        //     this.active = !this.active;
+        // }
+
+        // this.layoutService.onMenuStateChange({ key: this.key });
+    }
+}
