637de17826e054f5f05f2d0cdc0006f053a2d0d5
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-02-23 00:53:17 +0100

    hero + topbar

---
+++ src/app/pages/landing/components/herowidget/herowidget.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+
+@Component({
+    selector: 'hero-widget',
+    imports: [ButtonModule, RippleModule],
+    templateUrl: './app.herowidget.html'
+})
+export class HeroWidget {}
