099a0465722a9063caaafb856c12c4507ea61c80
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-03-15 15:05:23 +0100

    profile teacher for public

--- src/app/modules/profile/pages/profile/profile.component.ts
+++ src/app/modules/profile/pages/profile/profile.component.ts
@@ -1,10 +1,8 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { MenuItem } from 'primeng/api';
-import { UserResponseDTO } from '../../../../shared/models/user';
+import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../../shared/services/auth.service';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
-import { DividerClasses, DividerModule } from 'primeng/divider';
+import { DividerModule } from 'primeng/divider';
 import { AdressesListComponent } from '../../components/adresses-list/adresses-list.component';
 import { FormationsListComponent } from '../../components/formations-list/formations-list.component';
 import { PersonnalInfosComponent } from '../../components/personnal-infos/personnal-infos.component';
@@ -16,39 +14,7 @@ import { PersonnalInfosComponent } from '../../components/personnal-infos/person
     templateUrl: './profile.component.html',
     styleUrl: './profile.component.scss'
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
     authService = inject(AuthService);
     user = this.authService.userConnected;
-    ngOnInit(): void {}
-    items: MenuItem[] = [
-        {
-            label: 'Save',
-            icon: 'pi pi-check'
-        },
-        {
-            label: 'Update',
-            icon: 'pi pi-upload'
-        },
-        {
-            label: 'Delete',
-            icon: 'pi pi-trash'
-        },
-        {
-            label: 'Home Page',
-            icon: 'pi pi-home'
-        }
-    ];
-
-    userToDisplay: UserResponseDTO = {
-        id: '1',
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'email@email.com',
-        imgUrl: 'https://images.freeimages.com/365/images/previews/85b/psd-universal-blue-web-user-icon-53242.jpg',
-        dateOfBirth: new Date('1986-04-21'),
-        gender: 0,
-        lastLogginAt: new Date(),
-        emailConfirmed: true,
-        roles: ['Student', 'Teacher']
-    };
 }
---
+++ src/app/pages/landing/sub-pages/main/main.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TopbarWidget } from '../../components/topbar-landing/topbarwidget.component';
+import { HeroWidget } from '../../components/herowidget/herowidget';
+import { FeaturesWidget } from '../../components/features/featureswidget';
+import { HighlightsWidget } from '../../components/highlight/highlightswidget';
+import { FooterWidget } from '../../components/footerwidget/footerwidget';
+import { RippleModule } from 'primeng/ripple';
+import { StyleClassModule } from 'primeng/styleclass';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { TeacherPublicProfileComponent } from '../teacher-public-profile/teacher-public-profile.component';
+
+@Component({
+    selector: 'app-main',
+    imports: [RouterModule, FeaturesWidget, HeroWidget, HighlightsWidget, RippleModule, StyleClassModule, ButtonModule, DividerModule],
+
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.scss'
+})
+export class MainComponent {}
