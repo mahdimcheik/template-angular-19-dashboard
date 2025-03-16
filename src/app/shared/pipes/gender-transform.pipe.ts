06c8adc89ef24648d0e374d7e109bc1710c05fee
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-03-16 03:05:52 +0100

    comments

--- src/app/shared/pipes/gender-transform.pipe.ts
+++ src/app/shared/pipes/gender-transform.pipe.ts
@@ -1,22 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { EnumGender } from '../../shared/Models/user/user';
+import { EnumGender } from '../models/user';
 
 @Pipe({
-  name: 'genderTransform',
+    name: 'genderTransform'
 })
 export class GenderTransformPipe implements PipeTransform {
-  transform(genderNumber: number): string {
-    switch (genderNumber) {
-      case EnumGender.Homme:
-        return 'Homme';
-      case EnumGender.Femme:
-        return 'Femme';
-      case EnumGender.NonBinaire:
-        return 'NonBinaire';
-      case EnumGender.Autre:
-        return 'Autre';
-      default:
-        return 'Unknown';
+    transform(genderNumber: number): string {
+        switch (genderNumber) {
+            case EnumGender.Homme:
+                return 'Homme';
+            case EnumGender.Femme:
+                return 'Femme';
+            case EnumGender.NonBinaire:
+                return 'NonBinaire';
+            case EnumGender.Autre:
+                return 'Autre';
+            default:
+                return 'Unknown';
+        }
     }
-  }
 }
