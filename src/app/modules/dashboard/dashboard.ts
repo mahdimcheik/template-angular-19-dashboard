34ebd1d1c62fbbc2cf71d2ab5856e5338fd41e61
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-04-09 22:05:42 +0200

    cleaning

--- src/app/modules/dashboard/dashboard.ts
+++ src/app/modules/dashboard/dashboard.ts
@@ -1,7 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NotificationsWidget } from './components/notificationswidget';
 import { StatsWidget } from './components/statswidget';
-import { NotificationService } from '../../shared/services/notification.service';
 import { NotificationApp } from '../../shared/models/notification';
 
 @Component({
@@ -9,23 +8,4 @@ import { NotificationApp } from '../../shared/models/notification';
     imports: [StatsWidget, NotificationsWidget],
     templateUrl: './app.dashboard.html'
 })
-export class Dashboard implements OnInit {
-    offset = 0;
-    perPage = 10;
-    isread: boolean | undefined = undefined;
-
-    private notificationService = inject(NotificationService);
-    notifications: NotificationApp[] = [];
-
-    ngOnInit(): void {
-        // const filter = {
-        //     isRead: this.isread,
-        //     offset: this.offset,
-        //     perPage: this.perPage
-        // };
-        // this.notificationService.getNotificationsByUserId({ perPage: 100, offset: 0 }).subscribe((response: any) => {
-        //     console.log(response);
-        //     this.notifications = response.data?.items;
-        // });
-    }
-}
+export class Dashboard {}
