d6c72d27f6c58bdc5ae539130900f0d8c7ec88ab
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-07-07 17:47:36 +0200

    del gloable service

--- src/app/pages/landing/components/overlay-spinner/overlay-spinner.component.ts
+++ src/app/pages/landing/components/overlay-spinner/overlay-spinner.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, input } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
-import { GlobalService } from '../../../../shared/services/global.service';
 import { LoaderService } from '../../../../shared/services/loader.service';
 
 @Component({
@@ -11,7 +10,6 @@ import { LoaderService } from '../../../../shared/services/loader.service';
     styleUrl: './overlay-spinner.component.scss'
 })
 export class OverlaySpinnerComponent {
-    globalService = inject(GlobalService);
     loaderService = inject(LoaderService);
     isFetching = this.loaderService.isLoading;
 }
--- src/app/shared/interceptors/error-handler.interceptor.ts
+++ src/app/shared/interceptors/error-handler.interceptor.ts
@@ -1,23 +1,14 @@
-//
-
 import { inject } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
 import { catchError, finalize, switchMap, throwError } from 'rxjs';
-import { of } from 'rxjs';
-import { MessageService } from 'primeng/api';
 import { AuthService } from '../services/auth.service';
-import { LocalstorageService } from '../services/localstorage.service';
-import { GlobalService } from '../services/global.service';
 
 export const errorHandlerInterceptor: HttpInterceptorFn = (req, next) => {
     let isRefreshing = false;
     const authService = inject(AuthService);
-    const isFetching = inject(GlobalService).isFetching;
 
-    isFetching.set(true);
     // Ne pas intercepter les appels vers le refresh token
     if (req.url.includes('/refresh-token')) {
-        isFetching.set(false);
         return next(req);
     }
 
@@ -41,16 +32,11 @@ export const errorHandlerInterceptor: HttpInterceptorFn = (req, next) => {
                     }),
                     finalize(() => {
                         isRefreshing = false;
-                        isFetching.set(false);
                     })
                 );
             }
 
-            isFetching.set(false);
             return throwError(() => err);
-        }),
-        finalize(() => {
-            isFetching.set(false);
         })
     );
 };
