7cd488a41f33231f7b79dbd226f2d9e57f7e3835
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-08-03 18:02:59 +0200

    api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     // BACK_URL: 'https://localhost:7113',
     // BACK_URL: 'http://localhost:8080',
-    BACK_URL: 'https://skill-hive.fr',
+    BACK_URL: 'https://api.skill-hive.fr',
 
     // BACK_URL: 'https://192.168.1.19:7113',
     TEACHER_ID: '1'
