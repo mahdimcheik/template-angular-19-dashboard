62630dd345d4bb7e5a34ca59cf32ff1b1c3340c0
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-09-20 00:09:33 +0200

    fix : email

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,5 @@ export const environment = {
     // BACK_URL: 'https://192.168.1.19:7113',
     TEACHER_ID: '1',
     showTestCode: true,
-    teacherEmail: 'professeur@skill-hive.fr'
+    teacherEmail: 'teacher@skillhive.fr'
 };
